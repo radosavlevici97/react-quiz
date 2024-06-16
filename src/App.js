b9124ff026c7dbfd75d0ab66334c1eb777957b34
Daniel Radosavlevici <dradosavlevici@evolution.com>
2024-06-16 14:32:01 +0300

    feat: boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+
+export default function App() {
+  return (
+      <div className = "app">
+        <Header />
+
+        <Main>
+          <p>1/15</p>
+          <p>Question?</p>
+        </Main>
+  </div>
+  )
+}
